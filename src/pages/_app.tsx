7b92f67f8c24b15ae7b5bf7a382bb46fcd50bb64
Amish Verma <amishhverma@gmail.com>
2023-10-24 14:09:57 +0530

    put Head in _app for default metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,21 @@ import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 // import "@radix-ui/themes/styles.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     // <Theme accentColor="crimson" radius="large">
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Quack</title>
+        <meta
+          name="description"
+          content="Quack is a social media platform for quackers."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
