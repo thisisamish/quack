69a215803884c5040554a0fc7c6978db15049c12
Amish Verma <amishhverma@gmail.com>
2023-10-24 14:30:57 +0530

    profile page css

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { api } from "~/utils/api";
 import type { GetStaticProps } from "next";
 import { createServerSideHelpers } from "@trpc/react-query/server";
@@ -20,7 +21,18 @@ export default function ProfilePage({ username }: { username: string }) {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profilePicture}
+            alt={`${data.username}'s profile picture`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black"
+          ></Image>
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="w-full border-b border-slate-400"></div>
       </PageLayout>
     </>
   );
