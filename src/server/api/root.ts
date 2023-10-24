f72c79c300295060baba7dfb38fca3f3b5335534
Amish Verma <amishhverma@gmail.com>
2023-10-24 14:17:52 +0530

    new profile router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postsRouter } from "~/server/api/routers/posts";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { profileRouter } from "./routers/profile";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   posts: postsRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API
