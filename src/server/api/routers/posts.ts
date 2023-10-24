43d22e0294b7cd667a35b7d92524ac8e7f928c55
Amish Verma <amishhverma@gmail.com>
2023-10-24 16:55:42 +0530

    post page done

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -48,6 +48,20 @@ const rateLimit = new Ratelimit({
 });
 
 export const postsRouter = createTRPCRouter({
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const post = await ctx.db.post.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!post) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return (await addUserDataToPosts([post]))[0];
+    }),
+
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.db.post.findMany({
       take: 100,
